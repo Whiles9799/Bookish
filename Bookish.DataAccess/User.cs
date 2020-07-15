d137e7324d20d27df4eb4c49a3b6ba2d01afbbda
Ted While <t.while@live.co.uk>
2020-07-15 16:47:33 +0100

    Deletes unwanted class

--- Bookish.DataAccess/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bookish.DataAccess
-{
-    public class User
-    {
-        public int UserID { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
